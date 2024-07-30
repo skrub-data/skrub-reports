b546cd8d4d24d6f8d856e197b0254176b53e488d
jeromedockes <jeromedockes@users.noreply.github.com>
2024-07-30 17:49:45 +0000

    deploy: c5621dd5489136bd00486d7f0d97b2a8dfa7e498

--- skrub-online-report.js
+++ skrub-online-report.js
@@ -44,7 +44,7 @@ class App {
             this.handlers.push(new cls(elem, this));
         }
 
-        this.worker = new Worker("skrub-worker.js?version4cca");
+        this.worker = new Worker("skrub-worker.js?version681c");
         this.worker.onmessage = (e) => {
             this.receive(e.data);
         };
