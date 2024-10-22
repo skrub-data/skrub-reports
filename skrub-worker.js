ab4e3307c63ff3715e5cc5feb89c17e7fa48f0ad
Jerome Dockes <jerome@dockes.org>
2024-10-22 13:01:07 +0200

    update wheel

--- skrub-worker.js
+++ skrub-worker.js
@@ -13,7 +13,7 @@ async function startPyodide() {
     await self.pyodide.loadPackage("micropip");
     const micropip = self.pyodide.pyimport("micropip");
     await micropip.install(["pandas", "fastparquet", "matplotlib",
-        "skrub-0.3.1-py3-none-any.whl?__skrub_XXXX__",
+        "skrub-0.4.dev0-py3-none-any.whl?__skrub_XXXX__",
         "clevercsv-0.8.2-cp312-cp312-pyodide_2024_0_wasm32.whl"
     ]);
     await pyodide.runPython(`
